0ecd1f1940bf178c7e7d6b9b33a32f14b93666e3
quannm0105 <woongnguyen.15@gmail.com>
2021-02-07 17:53:25 +0700

    commit to done project 02

--- js/main.js
+++ js/main.js
@@ -1,120 +1,118 @@
 /*
 *    main.js
 *    Mastering Data Visualization with D3.js
-*    Project 1 - Star Break Coffee
+*    Project 2 - Gapminder Clone
 */
-
-let MARGIN = {LEFT: 100, TOP: 10, RIGHT: 20, BOTTOM: 100 };
-let WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
-let HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
-
-let flag = true
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
 const svg = d3.select("#chart-area").append("svg")
-    .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-    .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+	.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+	.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
 const g = svg.append("g")
-    .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+	.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
-    // x label
-    g.append("text")
-    .attr("class", "x axis-label")
-    .attr("x", WIDTH / 2)
-    .attr("y", HEIGHT + 60)
-    .attr("font-size","20px")
-    .attr("color", "#000")
-    .attr("text-anchor", "middle")
-    .text("Month");
+let time = 0
+
+// Scales
+const x = d3.scaleLog()
+	.base(10)
+	.range([0, WIDTH])
+	.domain([142, 150000])
+const y = d3.scaleLinear()
+	.range([HEIGHT, 0])
+	.domain([0, 90])
+const area = d3.scaleLinear()
+	.range([25*Math.PI, 1500*Math.PI])
+	.domain([2000, 1400000000])
+const continentColor = d3.scaleOrdinal(d3.schemePastel1)
+
+// Labels
+const xLabel = g.append("text")
+	.attr("y", HEIGHT + 50)
+	.attr("x", WIDTH / 2)
+	.attr("font-size", "20px")
+	.attr("text-anchor", "middle")
+	.text("GDP Per Capita ($)")
+const yLabel = g.append("text")
+	.attr("transform", "rotate(-90)")
+	.attr("y", -40)
+	.attr("x", -170)
+	.attr("font-size", "20px")
+	.attr("text-anchor", "middle")
+	.text("Life Expectancy (Years)")
+const timeLabel = g.append("text")
+	.attr("y", HEIGHT - 10)
+	.attr("x", WIDTH - 40)
+	.attr("font-size", "40px")
+	.attr("opacity", "0.4")
+	.attr("text-anchor", "middle")
+	.text("1800")
+
+// X Axis
+const xAxisCall = d3.axisBottom(x)
+	.tickValues([400, 4000, 40000])
+	.tickFormat(d3.format("$"))
+g.append("g")
+	.attr("class", "x axis")
+	.attr("transform", `translate(0, ${HEIGHT})`)
+	.call(xAxisCall)
+
+// Y Axis
+const yAxisCall = d3.axisLeft(y)
+g.append("g")
+	.attr("class", "y axis")
+	.call(yAxisCall)
 
 
-    // y label
-    const yLabel = g.append("text")
-    .attr("class", "y axis-label")
-    .attr("x", - (HEIGHT / 2))
-    .attr("y", -60)
-    .attr("font-size","20px")
-    .attr("color", "#000")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
+d3.json("data/data.json").then(function(data){
+	console.log(data)
 
+	// clear data
+	const formattedData = data.map( year => {
+		return year["countries"].filter(country => {
+			const dataExists = (country.income && country.life_exp)
+			return dataExists
+		}).map(country => {
+			country.income = Number(country.income)
+			country.life_exp = Number(country.life_exp)
+			return country
+		})
+	})
 
-    const x = d3.scaleBand()
-    .range([0, WIDTH])
-    .paddingInner(0.3)
-    .paddingOuter(0.3);
+	// run the code every 0.1 second
+	d3.interval(function(){
+		// at the end of our data, loop back
+		time = (time < 214) ? time + 1 : 0
+		update(formattedData[time])
 
-    const y = d3.scaleLinear()
-    .range([HEIGHT, 0]);
+	}, 100)
 
-    const xAxisGroup = g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", `translate(0, ${HEIGHT})`)
+	update(formattedData[0])
+})
 
-    const yAxisGroup = g.append("g")
-        .attr("class", "y axis")
+function update(data){
+	// standard transition time for the visualization
+	const t = d3.transition().duration(100)
 
+	// JOIN new data with old elements.
+	const circles = g.selectAll("circle")
+	.data(data, d => d.country)
 
-d3.json("data/revenues.json").then( function(data) {
-    data.forEach(function(d) {
-        d.revenue = Number(d.revenue)
-        d.profit = Number(d.profit)
-    });
+	// EXIT old elements not present in new data.
+	circles.exit().remove()
 
-    d3.interval(() => {
-        flag = !flag
-        const newData = flag ? data : data.slice(1)
-        update(newData)
-    }, 1000)
+	// ENTER new elements present in new data.
+	circles.enter().append("circle")
+	.attr("fill", d => continentColor(d.continent))
+	.merge(circles)
+	.transition(t)
+	.attr("cy", d => y(d.life_exp))
+	.attr("cx", d => x(d.income))
+	.attr("r", d => Math.sqrt(area(d.population) / Math.PI ))
 
-    update(data)
-});
-
-function update(data) {
-  const value = flag ? "profit" : "revenue"
-  const t = d3.transition().duration(750)
-
-  x.domain(data.map(d => d.month))
-  y.domain([0, d3.max(data, d => d[value])])
-
-  const xAxisCall = d3.axisBottom(x)
-  xAxisGroup.transition(t).call(xAxisCall)
-    .selectAll("text")
-      .attr("y", "10")
-      .attr("x", "-5")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-40)")
-
-  const yAxisCall = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + "m")
-  yAxisGroup.transition(t).call(yAxisCall)
-
-  // JOIN new data with old elements.
-  const rects = g.selectAll("rect")
-    .data(data, d => d.month)
-
-  // EXIT old elements not present in new data.
-  rects.exit()
-    .attr("fill", "red")
-    .transition(t)
-      .attr("height", 0)
-      .attr("y", y(0))
-      .remove()
-
-  // ENTER new elements present in new data...
-  rects.enter().append("rect")
-    .attr("fill", "grey")
-    .attr("y", y(0))
-    .attr("height", 0)
-    // AND UPDATE old elements present in new data.
-    .merge(rects)
-    .transition(t)
-      .attr("x", (d) => x(d.month))
-      .attr("width", x.bandwidth)
-      .attr("y", d => y(d[value]))
-      .attr("height", d => HEIGHT - y(d[value]))
-
-  const text = flag ? "Profit ($)" : "Revenue ($)"
-  yLabel.text(text)
+	// update the time label
+	timeLabel.text(String(time + 1800))
 }
